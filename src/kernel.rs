928c20a42304f06a79866c2f6be9a55a20a9814d
Nick Sanders <cogciprocate@gmail.com>
2016-02-13 14:57:41 -0800

    Clean up kernel.rs.

--- src/kernel.rs
+++ src/kernel.rs
@@ -40,18 +40,6 @@ impl Kernel {
     pub fn new(name: String, program: &Program, queue: &Queue, 
                 gws: WorkDims ) -> OclResult<Kernel>
     {
-        // let mut err: i32 = 0;
-
-        // let obj = unsafe {
-        //     cl_h::clCreateKernel(
-        //         program.obj(), 
-        //         ffi::CString::new(name.as_bytes()).unwrap().as_ptr(), 
-        //         &mut err
-        //     )
-        // };
-        
-        // let err_pre = format!("Ocl::create_kernel({}):", &name);
-        // raw::errcode_assert(&err_pre, err);
         let obj_raw = try!(raw::create_kernel(program.obj_raw(), &name));
 
         Ok(Kernel {
@@ -139,15 +127,6 @@ impl Kernel {
         //  TODO: ADD A CHECK FOR A VALID NAME (KEY)
         let arg_idx = self.named_args[name]; 
 
-        // raw::set_kernel_arg(
-        //     self.obj_raw,
-        //     arg_idx,
-        //     // mem::size_of::<T>() as size_t, 
-        //     // &scalar as *const _ as *const c_void,
-        //     KernelArg::Scalar(&scalar),
-        //     Some(&self.name),
-        // ).unwrap();
-
         self.set_arg::<T>(arg_idx, KernelArg::Scalar(&scalar)).unwrap();
     }
 
@@ -165,21 +144,13 @@ impl Kernel {
         };
 
         self.set_arg::<T>(arg_idx, KernelArg::Mem(buf_obj_raw)).unwrap();
-
-        // raw::set_kernel_arg::<T>(
-        //     self.obj_raw,
-        //     arg_idx,
-        //     // mem::size_of::<MemRaw>() as size_t, 
-        //     // (&buf_obj_raw.as_ptr() as *const *mut c_void) as *const c_void,
-        //     KernelArg::Mem(buf_obj_raw),
-        //     Some(&self.name),
-        // ).unwrap();
     }
 
     /// Enqueues kernel on the default command queue.
+    ///
+    /// [FIXME]: Implement non-default queue version.
     #[inline]
     pub fn enqueue(&self, wait_list: Option<&EventList>, dest_list: Option<&mut EventList>) {
-        // self.enqueue_with_cmd_queue(self.command_queue, wait_list, dest_list);
         raw::enqueue_kernel(self.command_queue, self.obj_raw.as_ptr(), self.gws.dim_count(), 
             self.gwo.as_raw(), self.gws.as_raw().unwrap(), self.lws.as_raw(), 
             wait_list.map(|el| el.events()), dest_list.map(|el| el.allot()), Some(&self.name));
@@ -191,17 +162,12 @@ impl Kernel {
         self.arg_count
     }    
 
-     // Non-builder-style version of `::arg_buf()`.
+    // Non-builder-style version of `::arg_buf()`.
     fn new_arg_buf<T: OclNum>(&mut self, buffer_opt: Option<&Buffer<T>>) -> u32 {
         let buf_obj = match buffer_opt {
             Some(buffer) => buffer.obj_raw(),
             None => MemRaw::null(),
         };
-
-        // self.new_kernel_arg(
-        //     mem::size_of::<MemRaw>() as size_t, 
-        //     (&buf.as_ptr() as *const *mut c_void) as *const c_void,
-        // )
 
         self.new_arg::<T>(KernelArg::Mem(buf_obj))
     }
@@ -213,31 +179,19 @@ impl Kernel {
             None => Default::default(),
         };
 
-        // self.new_arg::<T>(
-        //     mem::size_of::<T>() as size_t,
-        //     &scalar as *const _ as *const c_void,
-        //     //(scalar as *const super::cl_mem) as *const c_void,
-        // )
-
         self.new_arg::<T>(KernelArg::Scalar(&scalar))
     }
 
     // Non-builder-style version of `::arg_loc()`.
     fn new_arg_loc<T: OclNum>(&mut self, length: usize) -> u32 {
-        // self.new_kernel_arg(
-        //     (mem::size_of::<T>() * length) as size_t,
-        //     ptr::null(),
-        // )
-
         self.new_arg::<T>(KernelArg::Local(length))
     } 
 
     // Adds a new argument to the kernel and returns the index.
-    fn new_arg<T>(&mut self, arg: KernelArg<T> /*arg_size: size_t, arg_value: *const c_void*/) -> u32 {
+    fn new_arg<T>(&mut self, arg: KernelArg<T>) -> u32 {
         let arg_idx = self.arg_index;
 
         raw::set_kernel_arg::<T>(self.obj_raw, arg_idx, 
-                // KernelArg::Other { size: arg_size, value: arg_value }, 
                 arg,
                 Some(&self.name)
             ).unwrap();
@@ -248,25 +202,7 @@ impl Kernel {
 
     fn set_arg<T>(&self, arg_idx: u32, arg: KernelArg<T>) -> OclResult<()> {
         raw::set_kernel_arg::<T>(self.obj_raw, arg_idx, arg, Some(&self.name))
-    }
-
-    // // Modifies a kernel argument.
-    // // NOTE: Maintain mutability requirement to completely prevent simultaneous calls.
-    // fn set_kernel_arg(&mut self, arg_index: u32, arg_size: size_t, 
-    //             arg_value: *const c_void) 
-    // {
-    //     unsafe {
-    //         let err = cl_h::clSetKernelArg(
-    //                     self.obj_raw.as_ptr(), 
-    //                     arg_index,
-    //                     arg_size, 
-    //                     arg_value,
-    //         );
-
-    //         let err_pre = format!("ocl::Kernel::set_kernel_arg('{}'):", &self.name);
-    //         raw::errcode_assert(&err_pre, err);
-    //     }
-    // }    
+    } 
 }
 
 impl Drop for Kernel {
