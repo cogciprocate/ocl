a3abb9845dd641d42d4ea29700ada8b2b78569b5
Nick Sanders <cogciprocate@gmail.com>
2016-03-01 14:20:55 -0800

    Refiddle max workgroup functions. Iterate on examples.

--- mod.rs
+++ mod.rs
@@ -98,7 +98,7 @@ pub use self::functions::{ get_platform_ids, get_platform_info,
     enqueue_unmap_mem_object, enqueue_migrate_mem_objects, enqueue_kernel,
     enqueue_task, enqueue_native_kernel, enqueue_marker_with_wait_list,
     enqueue_barrier_with_wait_list, get_extension_function_address_for_platform,
-    get_max_work_group_size, wait_for_event, get_event_status, 
+    wait_for_event, get_event_status, 
     program_build_err, verify_context, get_first_platform};
 
 pub use self::cl::abs::{ClEventPtrNew, ClEventRef, ClPlatformIdPtr, ClDeviceIdPtr, EventRefWrapper,
