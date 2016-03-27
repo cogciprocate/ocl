cf74af3a21d850a7766df06ee7bcd149960cdb3b
Nick Sanders <cogciprocate@gmail.com>
2016-03-27 14:32:32 -0700

    Add runall.sh.

---
+++ examples/runall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cargo run --example basics
+cargo run --example events
+cargo run --example images
+cargo run --example img_formats
+cargo run --example info_core
+cargo run --example info
+cargo run --example threads
+cargo run --example timed
+cargo run --example trivial
