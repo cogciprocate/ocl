fbf63df83b76464b2cdcb4402393f6bef18e908d
Clement RENAULT <crenault@e3r1p4.42.fr>
2016-05-18 16:04:20 +0200

    added texture, renderbuffer from gl functions

--- mod.rs
+++ mod.rs
@@ -2,5 +2,6 @@ pub mod platform_h;
 pub mod cl_gl_h;
 
 pub use self::platform_h::{ ClGlUint, ClGlint, ClGlEnum };
-pub use self::cl_gl_h::{ clCreateFromGLBuffer, clEnqueueAcquireGLObjects,
+pub use self::cl_gl_h::{ clCreateFromGLBuffer, clCreateFromGLRenderbuffer,
+    clCreateFromGLTexture2D, clCreateFromGLTexture3D, clEnqueueAcquireGLObjects,
     clEnqueueReleaseGLObjects };
