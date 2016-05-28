bc8e8c8abcaf03951e86604148e5d9765a1b4ea0
Clément Renault <renault.cle@gmail.com>
2016-05-28 17:09:36 +0200

    WIP test cgl_sharing available

--- types/structs.rs
+++ types/structs.rs
@@ -9,7 +9,6 @@ use cl_h::{self, cl_mem};
 use core::{Mem, MemObjectType, ImageChannelOrder, ImageChannelDataType, ContextProperty,
         PlatformId};
 
-
 // Until everything can be implemented:
 pub type TemporaryPlaceholderType = ();
 
@@ -117,14 +116,21 @@ impl ContextProperties {
         self
     }
 
+    /// Specifies an OpenGL context CGL share group to
+    /// associate the OpenCL context with (builder-style).
+    pub fn cgl_sharegroup(mut self, gl_sharegroup: *mut libc::c_void) -> ContextProperties {
+        self.set_cgl_sharegroup(gl_sharegroup);
+        self
+    }
+
     /// Pushes a `ContextPropertyValue` onto this list of properties
     /// (builder-style).
-    pub fn property_value(mut self, prop: ContextPropertyValue) -> ContextProperties {   
+    pub fn property_value(mut self, prop: ContextPropertyValue) -> ContextProperties {
         self.set_property_value(prop);
         self
     }
 
-    /// Specifies a platfor.
+    /// Specifies a platform.
     pub fn set_platform<P: Into<PlatformId>>(&mut self, platform: P) {
         self.0.insert(ContextProperty::Platform, ContextPropertyValue::Platform(platform.into()));
     }
@@ -140,6 +146,12 @@ impl ContextProperties {
         self.0.insert(ContextProperty::GlContextKhr, ContextPropertyValue::GlContextKhr(gl_ctx));
     }
 
+    /// Specifies an OpenGL context CGL share group to
+    /// associate the OpenCL context with.
+    pub fn set_cgl_sharegroup(&mut self, gl_sharegroup: *mut libc::c_void) {
+        self.0.insert(ContextProperty::CglSharegroupKhr, ContextPropertyValue::CglSharegroupKhr(gl_sharegroup));
+    }
+
     /// Pushes a `ContextPropertyValue` onto this list of properties.
     pub fn set_property_value(&mut self, prop: ContextPropertyValue) {
         match prop {
@@ -147,15 +159,19 @@ impl ContextProperties {
                 self.0.insert(ContextProperty::Platform, ContextPropertyValue::Platform(val));
             },
             ContextPropertyValue::InteropUserSync(val) => {
-                self.0.insert(ContextProperty::InteropUserSync, 
+                self.0.insert(ContextProperty::InteropUserSync,
                     ContextPropertyValue::InteropUserSync(val));
             },
             ContextPropertyValue::GlContextKhr(val) => {
-                self.0.insert(ContextProperty::GlContextKhr, 
+                self.0.insert(ContextProperty::GlContextKhr,
                     ContextPropertyValue::GlContextKhr(val));
             },
+            ContextPropertyValue::CglSharegroupKhr(val) => {
+                self.0.insert(ContextProperty::CglSharegroupKhr,
+                    ContextPropertyValue::CglSharegroupKhr(val));
+            },
             _ => panic!("'{:?}' is not yet a supported variant.", prop),
-        }        
+        }
     }
 
     /// Returns a platform id or none.
@@ -192,6 +208,10 @@ impl ContextProperties {
                         props_raw.push(platform_id_core.as_ptr() as isize);
                     },
                     ContextPropertyValue::InteropUserSync(sync) => {
+                        props_raw.push(key.clone() as isize);
+                        props_raw.push(sync as isize);
+                    },
+                    ContextPropertyValue::CglSharegroupKhr(sync) => {
                         props_raw.push(key.clone() as isize);
                         props_raw.push(sync as isize);
                     },
