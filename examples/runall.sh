fda28562d37b0fbace035fbeafb5eef2ec7b97d2
Nick Sanders <cogciprocate@gmail.com>
2016-08-02 14:28:27 -0700

    Tweak runall script.

--- examples/runall.sh
+++ examples/runall.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-cargo run --example basics
-cargo run --example events
-cargo run --example images
-cargo run --example img_formats
-cargo run --example info_core
-cargo run --example info
-cargo run --example threads
-cargo run --example timed
-cargo run --example trivial
+cargo run --example basics "$@"
+cargo run --example events "$@"
+cargo run --example img_formats "$@"
+cargo run --example info_core "$@"
+cargo run --example info "$@"
+cargo run --example threads "$@"
+cargo run --example timed "$@"
+cargo run --example trivial "$@"
+
+cd examples/images
+cargo run "$@"
+cd -
