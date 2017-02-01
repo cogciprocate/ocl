ace96a74c4133ec9045e8c855273380355fa8195
Nick Sanders <cogciprocate@gmail.com>
2017-02-01 15:16:39 -0800

    Add new queue property values.

--- src/lib.rs
+++ src/lib.rs
@@ -313,6 +313,8 @@ bitflags! {
     pub flags CommandQueueProperties: u64 {
         const QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE = 1 << 0,
         const QUEUE_PROFILING_ENABLE = 1 << 1,
+        const QUEUE_ON_DEVICE = 1 << 2,
+        const QUEUE_ON_DEVICE_DEFAULT = 1 << 3,
     }
 }
 
