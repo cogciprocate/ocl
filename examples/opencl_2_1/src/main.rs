3d0d6bc594463d83c7fb961267f59fb1e18be141
Nick Sanders <cogciprocate@gmail.com>
2017-02-13 11:20:16 -0800

    2.1ex: tweak.

--- examples/opencl_2_1/src/main.rs
+++ examples/opencl_2_1/src/main.rs
@@ -9,12 +9,7 @@ static PLATFORM_NAME: &'static str = "Experimental OpenCL 2.1 CPU Only Platform"
 
 
 fn main() {
-    // let src = r#"
-    //     __kernel void add(__global float* buffer, float scalar) {
-    //         buffer[get_global_id(0)] += scalar;
-    //     }
-    // "#;
-
+    #[cfg(feature = "opencl_version_2_1")]
     let il_src: Vec<u8> = vec![
     // Magic number.           Version number: 1.0.
     0x03, 0x02, 0x23, 0x07,    0x00, 0x00, 0x01, 0x00,
@@ -24,8 +19,16 @@ fn main() {
     0x00, 0x00, 0x00, 0x00,
     // OpMemoryModel.          Logical.
     0x0e, 0x00, 0x03, 0x00,    0x00, 0x00, 0x00, 0x00,
-    // GLSL450.
-    0x01, 0x00, 0x00, 0x00];
+    // OpenCL.
+    0x02, 0x00, 0x00, 0x00];
+
+    #[cfg(not(feature = "opencl_version_2_1"))]
+    let src = r#"
+        __kernel void add(__global float* buffer, float scalar) {
+            buffer[get_global_id(0)] += scalar;
+        }
+    "#;
+
 
     #[cfg(feature = "opencl_version_2_1")]
     let platform = Platform::list().into_iter().find(|plat| plat.name() == PLATFORM_NAME)
@@ -34,19 +37,24 @@ fn main() {
     #[cfg(not(feature = "opencl_version_2_1"))]
     let platform = Platform::default();
 
+
     let device = Device::first(platform);
     let context = Context::builder()
         .platform(platform)
         .devices(device.clone())
         .build().unwrap();
 
-    // let program = Program::builder()
-    //     .devices(device)
-    //     .src(src)
-    //     .build(&context).unwrap();
+
+    #[cfg(feature = "opencl_version_2_1")]
     let program = Program::builder()
         .devices(device)
         .il(il_src)
+        .build(&context).unwrap();
+
+    #[cfg(not(feature = "opencl_version_2_1"))]
+    let program = Program::builder()
+        .devices(device)
+        .src(src)
         .build(&context).unwrap();
 
 
