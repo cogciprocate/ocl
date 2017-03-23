a66d2987b852da020e77c78758b50e796cb51285
Nick Sanders <cogciprocate@gmail.com>
2017-03-23 08:58:06 -0700

    Add convenience functions to `ProfilingInfoResult`.

--- src/types/enums.rs
+++ src/types/enums.rs
@@ -2284,6 +2284,38 @@ impl ProfilingInfoResult {
             Err(err) => ProfilingInfoResult::Error(Box::new(err)),
         }
     }
+
+    pub fn queued(self) -> OclResult<u64> {
+        match self {
+            ProfilingInfoResult::Queued(time_ns) => Ok(time_ns),
+            ProfilingInfoResult::Error(err) => Err(*err),
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn submit(self) -> OclResult<u64> {
+        match self {
+            ProfilingInfoResult::Submit(time_ns) => Ok(time_ns),
+            ProfilingInfoResult::Error(err) => Err(*err),
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn start(self) -> OclResult<u64> {
+        match self {
+            ProfilingInfoResult::Start(time_ns) => Ok(time_ns),
+            ProfilingInfoResult::Error(err) => Err(*err),
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn end(self) -> OclResult<u64> {
+        match self {
+            ProfilingInfoResult::End(time_ns) => Ok(time_ns),
+            ProfilingInfoResult::Error(err) => Err(*err),
+            _ => unreachable!(),
+        }
+    }
 }
 
 impl std::fmt::Debug for ProfilingInfoResult {
