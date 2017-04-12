811f817c593b17e449fe45464692735f12523f77
Nick Sanders <cogciprocate@gmail.com>
2017-04-11 22:10:08 -0700

    Add back examples.

--- examples_tmp/images_safe_clamp/target/debug/deps/ocl_example_rgb2gray-f8772758bc536cb5.d
+++
@@ -1,5 +0,0 @@
-/home/nick/src/github.com/cogciprocate/ocl/examples/images_safe_clamp/target/debug/deps/ocl_example_rgb2gray-f8772758bc536cb5: src/main.rs
-
-/home/nick/src/github.com/cogciprocate/ocl/examples/images_safe_clamp/target/debug/deps/ocl_example_rgb2gray-f8772758bc536cb5.d: src/main.rs
-
-src/main.rs:
