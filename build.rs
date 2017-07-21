8063113faedb78a379c3eeac97891057a415438f
pravic <ehysta@gmail.com>
2017-07-21 20:56:21 +0300

    Lookup for OpenCL import library under Windows.
    Fixes #28

--- build.rs
+++ build.rs
@@ -23,14 +23,27 @@
  */
 
 fn main() {
-    #[cfg(all(target_os= "windows", target_arch="x86", target_pointer_width="32"))]
-    {
-        //This is where the AMD OpenCL.lib file is located for RX480's
-        println!("cargo:rustc-link-search={}", r"C:\Program Files (x86)\AMD APP SDK\3.0\lib\x86\");
-    }
-
-    #[cfg(all(target_os= "windows", target_arch="x86_64", target_pointer_width="64"))]
-    {
-        println!("cargo:rustc-link-search={}", r"C:\Program Files (x86)\AMD APP SDK\3.0\lib\x86_64");
-    }
+	if cfg!(windows) {
+		// E.g. "c:\Program Files (x86)\Intel\OpenCL SDK\lib\x86\"
+		if let Ok(sdk) = std::env::var("INTELOCLSDKROOT") {
+			let mut path = std::path::PathBuf::from(sdk);
+			path.push("lib");
+			path.push(if cfg!(target_arch="x86_64") { "x64" } else { "x86"});
+			println!("cargo:rustc-link-search=native={}", path.display());
+		}
+		// E.g. "c:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\lib\Win32\"
+		if let Ok(sdk) = std::env::var("CUDA_PATH") {
+			let mut path = std::path::PathBuf::from(sdk);
+			path.push("lib");
+			path.push(if cfg!(target_arch="x86_64") { "x64" } else { "Win32"});
+			println!("cargo:rustc-link-search=native={}", path.display());
+		}
+		// E.g. "C:\Program Files (x86)\AMD APP SDK\3.0\lib\x86\"
+		if let Ok(sdk) = std::env::var("AMDAPPSDKROOT") {
+			let mut path = std::path::PathBuf::from(sdk);
+			path.push("lib");
+			path.push(if cfg!(target_arch="x86_64") { "x64_64" } else { "x86"});
+			println!("cargo:rustc-link-search=native={}", path.display());
+		}
+	}
 }
