e4dd43aff5ff6e3546b74b7e8a45ce7f252a7c22
pravic <ehysta@gmail.com>
2017-07-21 21:15:56 +0300

    Fix style in build script.

--- build.rs
+++ build.rs
@@ -23,24 +23,23 @@
  */
 
 fn main() {
-  if cfg!(windows) {
+    if cfg!(windows) {
+        let known_sdk = [ 
+            // E.g. "c:\Program Files (x86)\Intel\OpenCL SDK\lib\x86\"
+            ("INTELOCLSDKROOT", "x64", "x86"),
+            // E.g. "c:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\lib\Win32\"
+            ("CUDA_PATH", "x64", "Win32"),
+            // E.g. "C:\Program Files (x86)\AMD APP SDK\3.0\lib\x86\"
+            ("AMDAPPSDKROOT", "x64_64", "x86"),
+        ];
 
-    let known_sdk = [ 
-        // E.g. "c:\Program Files (x86)\Intel\OpenCL SDK\lib\x86\"
-        ("INTELOCLSDKROOT", "x64", "x86"),
-        // E.g. "c:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\lib\Win32\"
-        ("CUDA_PATH", "x64", "Win32"),
-        // E.g. "C:\Program Files (x86)\AMD APP SDK\3.0\lib\x86\"
-        ("AMDAPPSDKROOT", "x64_64", "x86"),
-       ];
-
-    for info in known_sdk.iter() {
-      if let Ok(sdk) = std::env::var(info.0) {
-        let mut path = std::path::PathBuf::from(sdk);
-        path.push("lib");
-        path.push(if cfg!(target_arch="x86_64") { info.1 } else { info.2 });
-        println!("cargo:rustc-link-search=native={}", path.display());
-      }
+        for info in known_sdk.iter() {
+            if let Ok(sdk) = std::env::var(info.0) {
+                let mut path = std::path::PathBuf::from(sdk);
+                path.push("lib");
+                path.push(if cfg!(target_arch="x86_64") { info.1 } else { info.2 });
+                println!("cargo:rustc-link-search=native={}", path.display());
+            }
+        }
     }
-  }
 }
