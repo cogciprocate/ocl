aed9d45d1ea2b41dd2c6f5e3f71fdbf4d8cbb7fe
pravic <ehysta@gmail.com>
2017-07-21 21:12:20 +0300

    Refactor OpenCL SDK lookup in build script.

--- build.rs
+++ build.rs
@@ -24,26 +24,23 @@
 
 fn main() {
 	if cfg!(windows) {
-		// E.g. "c:\Program Files (x86)\Intel\OpenCL SDK\lib\x86\"
-		if let Ok(sdk) = std::env::var("INTELOCLSDKROOT") {
-			let mut path = std::path::PathBuf::from(sdk);
-			path.push("lib");
-			path.push(if cfg!(target_arch="x86_64") { "x64" } else { "x86"});
-			println!("cargo:rustc-link-search=native={}", path.display());
-		}
-		// E.g. "c:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\lib\Win32\"
-		if let Ok(sdk) = std::env::var("CUDA_PATH") {
-			let mut path = std::path::PathBuf::from(sdk);
-			path.push("lib");
-			path.push(if cfg!(target_arch="x86_64") { "x64" } else { "Win32"});
-			println!("cargo:rustc-link-search=native={}", path.display());
-		}
-		// E.g. "C:\Program Files (x86)\AMD APP SDK\3.0\lib\x86\"
-		if let Ok(sdk) = std::env::var("AMDAPPSDKROOT") {
-			let mut path = std::path::PathBuf::from(sdk);
-			path.push("lib");
-			path.push(if cfg!(target_arch="x86_64") { "x64_64" } else { "x86"});
-			println!("cargo:rustc-link-search=native={}", path.display());
+
+		let known_sdk = [ 
+								// E.g. "c:\Program Files (x86)\Intel\OpenCL SDK\lib\x86\"
+								("INTELOCLSDKROOT", "x64", "x86"),
+								// E.g. "c:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\lib\Win32\"
+								("CUDA_PATH", "x64", "Win32"),
+								// E.g. "C:\Program Files (x86)\AMD APP SDK\3.0\lib\x86\"
+								("AMDAPPSDKROOT", "x64_64", "x86"),
+								 ];
+
+		for info in known_sdk.iter() {
+			if let Ok(sdk) = std::env::var(info.0) {
+				let mut path = std::path::PathBuf::from(sdk);
+				path.push("lib");
+				path.push(if cfg!(target_arch="x86_64") { info.1 } else { info.2 });
+				println!("cargo:rustc-link-search=native={}", path.display());
+			}
 		}
 	}
 }
