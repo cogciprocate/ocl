976de1e4425ce2f0c07318cd28eaa28ac92f7015
Lee McCarthy <imjustagan@gmail.com>
2017-07-27 10:25:14 -0700

    incomplete glium test

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,6 +1,8 @@
 extern crate ocl_interop;
 extern crate ocl;
 extern crate gl;
+#[macro_use]
+extern crate glium;
 
 extern crate sdl2;
 extern crate glfw;
@@ -149,6 +151,7 @@ impl TestContent for CLMultiplyByScalar{
     fn clean_up(&mut self){
     }
 }
+
 struct CLGenVBO{
     gl_buff: GLuint,
     gl_program: GLuint,
@@ -475,4 +478,73 @@ fn glutin_works() {
     }
     gl_window.hide();
     thing.clean_up();
+}
+
+fn glium_works(){
+
+    use glium::Surface;
+
+    let mut events_loop = glutin::EventsLoop::new();
+    let window = glutin::WindowBuilder::new()
+        .with_title("Glutin Window with glium");
+    let context = glutin::ContextBuilder::new();
+    let display = glium::Display::new(window, context, &events_loop).unwrap();
+
+    #[derive(Copy, Clone)]
+    struct Vertex {
+        position: [f32; 2],
+    }
+
+    implement_vertex!(Vertex, position);
+
+    let vertex1 = Vertex { position: [-0.5, -0.5] };
+    let vertex2 = Vertex { position: [ 0.0,  0.5] };
+    let vertex3 = Vertex { position: [ 0.5, -0.25] };
+    let shape = vec![vertex1, vertex2, vertex3];
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    let vertex_shader_src = r#"
+        #version 140
+
+        in vec2 position;
+
+        void main() {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 140
+
+        out vec4 color;
+
+        void main() {
+            color = vec4(0.0, 0.0, 0.0, 1.0);
+        }
+    "#;
+
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
+    let mut running = true;
+    let mut frame_count=0;
+    while running && frame_count < MAX_FRAME_COUNT {
+        let mut target = display.draw();
+        target.clear_color(0.33333, 0.0, 1.0, 1.0);
+        target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms,
+                    &Default::default()).unwrap();
+        target.finish().unwrap();
+
+        events_loop.poll_events(|event| {
+            match event {
+                glutin::Event::WindowEvent { event, .. } => match event {
+                    glutin::WindowEvent::Closed => running = false,
+                    _ => ()
+                },
+                _ => (),
+            }
+        });
+        frame_count+=1;
+    }
 }
