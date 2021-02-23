5d7bf4f7b82a31599a7c306367a4d81525f9ea0f
Michael Mattig <mattig@mathematik.uni-marburg.de>
2021-02-23 22:01:20 +0100

    update rustc_version

--- ocl-core/build.rs
+++ ocl-core/build.rs
@@ -1,10 +1,11 @@
-
-
 extern crate rustc_version;
 // use rustc_version::{version, version_matches, version_meta, Channel};
 
 fn main() {
-    assert!(rustc_version::version_matches(">= 1.13.0"), "This crate has been recently redesigned
+    assert!(
+        rustc_version::version().unwrap() >= rustc_version::Version::new(1, 13, 0),
+        "This crate has been recently redesigned
         to take advantage of MIR and requires versions of the compiler which do not create hidden
-        drop flag fields on #[repr(C)] structs.");
+        drop flag fields on #[repr(C)] structs."
+    );
 }
