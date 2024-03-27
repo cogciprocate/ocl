36c77a53d82a7b7235aa3b175e411ab6584e65c9
Nick Sanders <nsan1129@gmail.com>
2024-03-27 10:09:45 -0700

    Version 0.19.7

--- ocl/src/lib.rs
+++ ocl/src/lib.rs
@@ -44,7 +44,7 @@
 //! [`Result`]: /ocl/ocl/type.Result.html
 //! [examples]: https://github.com/cogciprocate/ocl/tree/master/examples
 
-#![doc(html_root_url = "https://docs.rs/ocl/0.19.6")]
+#![doc(html_root_url = "https://docs.rs/ocl/0.19.7")]
 
 // #![warn(missing_docs)]
 
